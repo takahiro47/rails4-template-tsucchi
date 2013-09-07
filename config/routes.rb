584288fc17f9ae2a3d876541dea053bd71cbf10d
Takahiro Tsuchiya <tsucchi@ht.sfc.keio.ac.jp>
2013-09-08 00:13:37 +0900

    create pages#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Cookpad::Application.routes.draw do
+
+  get "pages/index"
+  root 'pages#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +43,7 @@ Cookpad::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'

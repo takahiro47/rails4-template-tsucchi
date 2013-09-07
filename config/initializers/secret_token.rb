051155a99358b1736fce05fb6476b879944e938c
Takahiro Tsuchiya <tsucchi@ht.sfc.keio.ac.jp>
2013-09-07 22:23:56 +0900

    first commit(rails4 RAW)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cookpad::Application.config.secret_key_base = 'e37c169f8dfbe7f7845f0c9b6e4f02128c239d62c0c2667297a62a24c513df225e790d674926d731e41a2f8067ed0b423349c2261273128155287a765e051686'
